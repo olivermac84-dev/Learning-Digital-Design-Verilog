5adffea5f6e41d1ee7472d1d9591a268f46c8169
SithumZ <olivermac84@gmail.com>
2026-06-19 23:53:17 +0530

    Update full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -1,13 +1,13 @@
 module full_adder(
     input a,
     input b,
-    input cin,     // Carry In (කලින් ඉතිරි වුණු අගය)
+    input cin,     
     output sum,
-    output cout    // Carry Out (දැන් ඉතිරි වන අගය)
+    output cout    
 );
 
-    // Full Adder එකේ සූත්‍රය (Logic equations)
+   
     assign sum = a ^ b ^ cin;
-    assign cout = (a & b) | (b & cin) | (cin & a); // | කියන්නේ OR එකට
+    assign cout = (a & b) | (b & cin) | (cin & a); 
 
 endmodule
